6ab4d72852dc2cc66b9018e4718a6f0b4c1bb6e1
suolenkainen <pmarjama@gmail.com>
2022-09-25 17:24:20 +0300

    CSVs to dicts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# market
+# Market
+This is a simulation about market.
+
+NOTE: utils.py is a PoC of unittest
+
+CSVs to dicts: Created util function to convert csv-files to dicts
+
+TODO:
+- Create people with needs
+- Create a product
+- Create a supplier with production
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Author: Pekka Marjamäki - Suolenkainen
+# https://github.com/suolenkainen/market
+
+from subprocess import check_output
+import random
+import csv
+import os
+
+
+def random_item_from_list(list):
+    _random_item = random.choice(list)
+    return _random_item + 1
+
+
+def csv_to_dict_converter(filename):
+    with open(filename, newline='') as csvfile:
+        file_data=csv.reader(csvfile)
+        headers=next(file_data)
+        _result_dict = [dict(zip(headers,i)) for i in file_data]
+    return _result_dict
+    
+
+if __name__ == '__main__':
+    check_output("python .\\src\\utils.py.test -v", shell=True)
