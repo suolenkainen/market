6b02dab3bb79b2c1cc627ae579c50f7d47708662
suolenkainen <pmarjama@gmail.com>
2022-10-02 13:56:44 +0300

    Type converting from csv

--- src/utils.py
+++ src/utils.py
@@ -3,22 +3,30 @@
 # https://github.com/suolenkainen/market
 
 from subprocess import check_output
-import random
 import csv
-
-
-def random_item_from_list(list):
-    _random_item = random.choice(list)
-    return _random_item + 1
+from re import match as regmatch
 
 
 def csv_to_dict_converter(filename):
     with open(filename, newline='') as csvfile:
         file_data=csv.reader(csvfile)
         headers=next(file_data)
-        _result_dict = [dict(zip(headers,i)) for i in file_data]
+        _result_list_dicts = [dict(zip(headers,i)) for i in file_data]
     csvfile.close()
-    return _result_dict
+
+    for _dict in _result_list_dicts:
+        for key, value in _dict.items():
+            
+            # Converts integers to proper form
+            reg = r"[0-9]"
+            if regmatch(reg, value):
+                _dict[key] = float(value)
+
+            # Converts boolean to proper form
+            if value in ["True", "False"]:
+                _dict[key] = bool(value)
+
+    return _result_list_dicts
     
 
 if __name__ == '__main__':
