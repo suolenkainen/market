9281230a5fa6adfcbb7ac9130f04783716e745a3
suolenkainen <pmarjama@gmail.com>
2022-11-03 21:06:28 +0200

    added clarity to csv-files

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 
 from data import PEOPLE, PURCHASE_ORDERS, SALES_ORDERS, MARKET_PRICES
 from data import PEOPLE_ID, PURCHASE_ORDERS_ID, SALES_ORDERS_ID
-import transactions, people, needs, production
+import transactions, people, needs
 
 
 # Handle transactions -> matches, and purchases and sales in dict form
@@ -23,37 +23,3 @@ print()
 _people = people.adjust_person_wealth(PEOPLE, _people_transactions)
 print(_people)
 print()
-
-
-# Add products to people
-_people_products = people.add_products_to_people(PEOPLE_PRODUCT, _people_transactions)
-print(_people_products)
-print()
-
-
-# Apply needs and deactivate
-_people_products, _needs = needs.people_consume_products_and_generate_needs(_people_products, NEEDS)
-print(_people_products, _needs)
-print()
-
-_people, _needs = needs.deactivate_person(_people, _needs)
-print(_people, _needs)
-print()
-
-
-# Adjust needs prices
-_needs = needs.adjust_need_prices(_needs, _purchase_dict)
-print(_needs)
-print()
-
-
-# Generate products for producers
-_producers, _storage = production.generate_products_for_producers(PRODUCERS, STORAGE)
-print(_producers, _storage)
-print()
-
-
-# Generate new sales orders based on warehouse and production
-
-
-# Generate new purchase orders based on needs
